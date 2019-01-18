1f9eb20ed7c528c6ef17c0b12c42d533e50c5eb6
Sergey <seryoja.mashencko@yandex.ru>
2019-01-18 17:58:34 +0300

    Ver. 0.5

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,37 @@
-$(".nav-link a").on("click", function(){
-  $(".nav-link").find(".active").removeClass("active");
-  $(this).parent().addClass("active");
+import * as jQuery from 'bootstrap';
+
+$('.nav-link a').on('click', function(){
+  $('.nav-link').find('.active').removeClass('active');
+  $(this).parent().addClass('active');
 });
 
 
-$('body').scrollspy({target: ".collapse"})
+$('body').scrollspy({target: '.collapse'});
+////
+// const $win = $(window),
+//   $fixed = $('.active'),
+//   limit = 600;
+//
+// function tgl (state) {
+//   $fixed.toggleClass('no-active', state);
+// }
+//
+// $win.on('scroll', function () {
+//   const top = $win.scrollTop();
+//
+//   if (top < limit) {
+//     tgl(true);
+//   } else {
+//     tgl(false);
+//   }
+// });
+jQuery(document).ready(function() {
+  jQuery('no-active').addClass('hidden').viewportChecker({
+    classToAdd: 'active-in visible',
+    offset: 100
+  });
+});
+////
 
 ///
 const email = document.getElementById('mail');
